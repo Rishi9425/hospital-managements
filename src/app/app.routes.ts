9fc6abef9e8ebbfc3eb3332c2db886a5af630292
Rishi9425 <gs2115025@sgsitsindore.in>
2025-04-16 17:44:58 +0530

    My first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { DoctorCardsComponent } from './doctor-cards/doctor-cards.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {
+path: 'signup', component: SignupComponent
+    },
+{
+    path: '', component: DoctorCardsComponent
+}
+
+];
---
+++ src/app/doctor-cards/doctor-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-doctor-cards',
+  imports: [],
+  templateUrl: './doctor-cards.component.html',
+  styleUrl: './doctor-cards.component.css'
+})
+export class DoctorCardsComponent {
+
+}
